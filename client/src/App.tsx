2b28b088fb416a3145123a5f3b66c379665b7483
Randy-diebold <randy.diebold@gmail.com>
2021-10-28 11:41:04 -0700

    added global variables on client, added nodemon and .env file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 import HomeContainer from './Containers/HomeContainer';
+import { serverpath } from './globalVariables';
 
 function App() {
   return (
     <div>
+      <h1>{serverpath}</h1>
       <h1>Rendereing from APP.tsx</h1>
-      <HomeContainer /> 
+      <HomeContainer />
     </div>
-  )
+  );
 }
 
 export default App;
