cde7a9019d427ecfff719c6dd8577d2e81d77317
Randy-diebold <randy.diebold@gmail.com>
2021-10-27 18:07:10 -0700

    new init

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('ping server');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
