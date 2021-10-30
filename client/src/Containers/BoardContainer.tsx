6be132df22e66ce72dc44a478493e04ef80fea4d
Ian Judd <ianjudd@Ians-iMac.local>
2021-10-30 17:48:19 -0400

    bulletin board frontend optimized

--- client/src/Containers/BoardContainer.tsx
+++ client/src/Containers/BoardContainer.tsx
@@ -5,7 +5,7 @@ import SearchBar from "../Components/SearchBar";
 const BoardContainer = () => {
 
     return (
-        <div className='flex flex-col h-full w-1/2'>
+        <div className='flex flex-col h-full'>
          
                 <div className='row-start-1 bg-white row-start-1 row-span-{3} relative rounded-r-3xl'>
             <h1 className="flex items-center justify-center text-xl font-bold h-20 bg-yellow-default">BULLETIN BOARD </h1>
@@ -14,7 +14,7 @@ const BoardContainer = () => {
                  <SearchBar />
                 </div>
 
-            <div className='flex items-center text-lg font-medium sticky top-0 bg-white mr-5 w-full'></div>
+            {/* <div className='overflow-y-scroll flex items-center text-lg font-medium sticky top-0 bg-white mr-5 w-full'></div> */}
             <BulletinBoard />
             </div>
         </div>
