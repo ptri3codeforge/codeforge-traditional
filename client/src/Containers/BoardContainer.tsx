64da43d99a6fb8d81cdbecc62f31595aa31c52db
Charlie Malave <malavecharles@gmail.com>
2021-11-01 16:35:21 -0400

    touching up bulletin board

--- client/src/Containers/BoardContainer.tsx
+++ client/src/Containers/BoardContainer.tsx
@@ -5,12 +5,12 @@ import SearchBar from "../Components/SearchBar";
 const BoardContainer = () => {
 
     return (
-        <div className='flex flex-col h-full'>
+        <div className='flex flex-col h-full rounded-r-3xl bg-yellow-default rounded-r-3xl h-full'>
          
-                <div className='row-start-1 bg-white row-start-1 row-span-{3} relative rounded-r-3xl'>
-            <h1 className="flex items-center justify-center text-xl font-bold h-20 bg-yellow-default">BULLETIN BOARD </h1>
+            <div className='row-start-1 row-start-1 row-span-{3}  relative rounded-r-3xl'>
+                 <h1 className="flex items-center bg-yellow-default justify-center text-xl font-bold h-20 shadow-md ">BULLETIN BOARD </h1>
             
-                <div className="flex items-center justify-center h-20 w-full bg-white">
+                <div className="flex items-center justify-center h-20 w-full bg-white-default">
                  <SearchBar />
                 </div>
 
